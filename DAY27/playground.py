423c9b0c1d8bc64cebfc9d4dd112d03ae785dd06
Thuong180702 <vanthuong180702t@gmail.com>
2022-09-21 11:40:39 +0700

    Update Day 27

---
+++ DAY27/playground.py
@@ -0,0 +1,28 @@
+def add(*args):
+
+    sum = 0
+    for n in args:
+        sum += n
+    return sum
+
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.color = kw.get("color")
+        self.seats = kw.get("seats")
+
+
+my_car = Car(make="Nissan", model="Skyline")
+print(my_car.model)
